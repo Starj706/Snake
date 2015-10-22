2661c5bf512bdaa7b1e2f57570714e3e08e01a05
Starj706 <starj706@yandex.ru>
2015-10-22 15:07:03 +0400

    Доблавлен класс Point

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,17 +10,17 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sim = '*';
+            p1.Draw();
+            
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 6;
+            p2.sim = '#';
+            p2.Draw();
 
             Console.ReadLine();
         }
