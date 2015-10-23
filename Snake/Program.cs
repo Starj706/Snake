bdf9678ebf313c37d55668d1c4143962b3004859
Starj706 <starj706@yandex.ru>
2015-10-23 14:09:40 +0400

    Опыты со списками

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,12 +11,30 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 2, '*');
-           
             p1.Draw();
             
             Point p2 = new Point(4, 6, '#');
-           
             p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = List < Point();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
