2aa2debfeb0fecee3cc5144ac6fc43f572d7ddaf
Starj706 <starj706@yandex.ru>
2015-10-23 15:03:23 +0400

    Добавлены классы горизонтальных и вертикальных линий

---
+++ Snake/VertikalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VertikalLine
+    {
+        List<Point> pList;
+
+        public VertikalLine(int x, int yTop, int yDown, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yTop; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }
+
+        public void DrowV()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
